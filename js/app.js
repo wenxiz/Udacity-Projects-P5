96f3b1d2d811b237fbc12dd2ce31c20a2b49e5ca
Wenxi Zhou <vic.zwx1990@gmail.com>
2017-09-29 12:17:07 -0700

    1. optimize effect of bugs animation 2. set reset() function to reset player's ordinate 3. fixed bug that player can cross bugs horizonally 4. avoided Magic number by set cell_width and cell_heigh variable 5. added alert in failure scenario 6. added failure alert into README.md

--- js/app.js
+++ js/app.js
@@ -9,6 +9,9 @@ var Enemy = function(x, y, speed) {
     this.sprite = 'images/enemy-bug.png';
 };
 
+// 定义图片大小
+var cell_width = 101;
+    cell_height = 83;
 // 此为游戏必须的函数，用来更新敌人的位置
 // 参数: dt ，表示时间间隙
 Enemy.prototype.update = function(dt, x) {
@@ -16,8 +19,8 @@ Enemy.prototype.update = function(dt, x) {
     // 都是以同样的速度运行的
     this.x += dt * this.speed
     //敌人不断从屏幕左边出现
-    if (this.x >= 505) {
-        this.x = -30;
+    if (this.x >= 5 * cell_width) {
+        this.x = -1 * cell_width;
     }
 };
 
@@ -34,29 +37,36 @@ var Player = function(x, y) {
     this.sprite = 'images/char-boy.png';
 };
 
+// 解决到达河岸再reset，延时出现alert
 var count = 0
 Player.prototype.update = function(dt) {
     if (this.y === -11) {
         count ++;
         if (count % 5 === 2) {
             alert("You Win!!");
-            this.x = 202;
-            this.y = 404;
+            this.reset();
         }
     } 
 };
 
-//碰撞检测
+Player.prototype.reset = function(){
+    this.x = 2 * cell_width;
+    this.y = 4 * cell_width;
+}
+// 碰撞检测
 Player.prototype.checkCollisions = function () {
     for(var i=0;i<allEnemies.length;i++){
         //首先判断player和enemy是否在同一行（
         if(this.y === allEnemies[i].y){
             //再判断两个图形间的距离
-            if((Math.abs(this.x - allEnemies[i].x))<40){
-                this.x =200;
-                this.y =404;
+            if((Math.abs(this.x - allEnemies[i].x))<70){
+                count ++;
+                if (count % 5 === 2) {
+                    alert("Oops, you got caught!");
+                    this.reset();
+                }
+                
     // console.log(`checkCollisions() is working: player: ${this.x}, ${this.y}; enemy: ${allEnemies[i].x}, ${allEnemies[i].y}`)
-
   }
 }
 }
@@ -68,19 +78,19 @@ Player.prototype.handleInput = function(movement) {
     switch(movement) {
         case 'left':  
         if (this.x > 0) {
-            this.x -= 101;}
+            this.x -= cell_width;}
         break;
         case 'right': 
-        if (this.x < 404) {
-            this.x += 101;}
+        if (this.x < 4 * cell_width) {
+            this.x += cell_width;}
         break;
         case 'up': 
         if (this.y > 0) {
-            this.y -= 83;}
+            this.y -= cell_height;}
         break;
         case 'down': 
-        if (this.y < 389) {
-            this.y += 83;}
+        if (this.y < 4 * cell_height) {
+            this.y += cell_height;}
         break;
     }
 };
@@ -97,10 +107,10 @@ Player.prototype.render = function() {
 var allEnemies = [];
 // 加入bugs
 for (var i=0; i<6; i++){
-    var bugs = new Enemy(-30, 83*(i%3)+72);
+    var bugs = new Enemy(-1 * cell_width, cell_height*(i%3)+72);
     allEnemies.push(bugs);
-};
-var player = new Player(202, 404)
+}
+var player = new Player(2 * cell_width, 4 * cell_width)
 
 // 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
 // 方法里面。你不需要再更改这段代码了。
