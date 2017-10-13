26226b6be7f0deef131033f63aacda5fd76d4c08
Wenxi Zhou <vic.zwx1990@gmail.com>
2017-10-13 11:19:18 -0700

    1. Add score function 2. rewrite comment and READ.ME

--- js/app.js
+++ js/app.js
@@ -1,50 +1,67 @@
-// 这是我们的玩家要躲避的敌人 
+// Define the size of image and scores
+var cell_width = 101,
+    cell_height = 83,
+    floatingScore = {
+    water: +100,
+    enemy: -200
+};
+
+
+// Enemies our player must avoid 
 var Enemy = function(x, y, speed) {
-    // 要应用到每个敌人的实例的变量写在这里
-    // 我们已经提供了一个来帮助你实现更多
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
     this.x = x;
     this.y = y;
     this.speed = speed || Math.random() * 100 +100;
-    // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
-// 定义图片大小
-var cell_width = 101;
-    cell_height = 83;
-// 此为游戏必须的函数，用来更新敌人的位置
-// 参数: dt ，表示时间间隙
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt, x) {
-    // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
-    // 都是以同样的速度运行的
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
     this.x += dt * this.speed
-    //敌人不断从屏幕左边出现
+    // Enemis appear from left of screen
     if (this.x >= 5 * cell_width) {
         this.x = -1 * cell_width;
     }
 };
 
-// 此为游戏必须的函数，用来在屏幕上画出敌人，
+// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// 现在实现你自己的玩家类
-// 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
 var Player = function(x, y) {
     this.x = x;
     this.y = y;
-    this.sprite = 'images/char-boy.png';
+    var roleImages = [
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png'];
+    var index = Math.floor(Math.random() * roleImages.length);
+    this.sprite = roleImages[index];
+    this.score = 0;
 };
 
-// 解决到达河岸再reset，延时出现alert
+// Give user a delay time to realize their control has been done
 var count = 0
 Player.prototype.update = function(dt) {
     if (this.y === -11) {
         count ++;
         if (count % 5 === 2) {
-            alert("You Win!!");
+            // alert("You Win!!");
             this.reset();
+            this.score += Math.abs(floatingScore.water);
         }
     } 
 };
@@ -53,26 +70,28 @@ Player.prototype.reset = function(){
     this.x = 2 * cell_width;
     this.y = 4 * cell_width;
 }
-// 碰撞检测
+// Collision detection
 Player.prototype.checkCollisions = function () {
     for(var i=0;i<allEnemies.length;i++){
-        //首先判断player和enemy是否在同一行（
+        //First check player and enemies' x coordinates（
         if(this.y === allEnemies[i].y){
-            //再判断两个图形间的距离
+            //then check the distance between player and enemies
             if((Math.abs(this.x - allEnemies[i].x))<70){
                 count ++;
                 if (count % 5 === 2) {
-                    alert("Oops, you got caught!");
+                    // alert("Oops, you got caught!");
+                    if (this.score >= Math.abs(floatingScore.enemy)) {
+                        this.score -= Math.abs(floatingScore.enemy);
+                    } else {
+                        this.score = 0;
+                    }
                     this.reset();
                 }
-                
     // console.log(`checkCollisions() is working: player: ${this.x}, ${this.y}; enemy: ${allEnemies[i].x}, ${allEnemies[i].y}`)
-  }
+            }
+        }
+    }
 }
-}
-}
-
-
 
 Player.prototype.handleInput = function(movement) {
     switch(movement) {
@@ -95,25 +114,25 @@ Player.prototype.handleInput = function(movement) {
     }
 };
 
-//此函数用来屏幕上出现玩家
+//This function is used to render player
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// 现在实例化你的所有对象
-// 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
-// 把玩家对象放进一个叫 player 的变量里面
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
 var allEnemies = [];
-// 加入bugs
+// Put in bugs
 for (var i=0; i<6; i++){
-    var bugs = new Enemy(-1 * cell_width, cell_height*(i%3)+72);
+    var bugs = new Enemy(-1 * cell_width, cell_height*(i%4)+72);
     allEnemies.push(bugs);
 }
 var player = new Player(2 * cell_width, 4 * cell_width)
 
-// 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
-// 方法里面。你不需要再更改这段代码了。
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -124,3 +143,4 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
